f28e7ca94d18adda34160117799f5e0374508319
donald_p <donaldpakkies@gmail.com>
2019-06-11 12:22:48 +0200

    feat: added a error message component
    responsible for displaying error messages

---
+++ resources/js/components/ErrorMessages.vue
@@ -0,0 +1,15 @@
+<<template>
+  <div class="invalid-feedback" role="alert">
+    <span v-for="message in error">
+      <strong>{{ message }}</strong>
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'error'
+    ]
+  }
+</script>
