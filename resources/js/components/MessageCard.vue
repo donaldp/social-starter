dfc057a64078653d17326dc81b577c2ba6ab2272
donald_p <donaldpakkies@gmail.com>
2019-06-11 12:23:30 +0200

    feat: added a message card component
    responsible for displaying posts (messages)

---
+++ resources/js/components/MessageCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card shadow-sm" style="margin: 15px 0">
+
+    <div class="card-header">
+      <div class="d-flex justify-content-between align-items-center">
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="mr-2">
+            <img class="rounded-circle" width="45" :src="post.user.gravatar" alt="">
+          </div>
+          <div class="ml-2">
+            <div class="h7 text-muted">{{ post.user.name }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card-body">
+      <div class="text-muted h7 mb-2">
+        <vue-moments-ago prefix="" suffix="ago" :date="post.created_at"></vue-moments-ago>
+      </div>
+
+      <p class="card-text">
+        {{ post.body }}
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import VueMomentsAgo from 'vue-moments-ago';
+
+  export default {
+    props: [
+      'post'
+    ],
+    components: {
+      VueMomentsAgo
+    }
+  }
+
+</script>
